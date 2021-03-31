5a50e1fee10c24627307562259c2af3c0ea77a0f
Administrator <admin@example.com>
2021-03-31 11:37:20 +0700

    commit ke ketiga

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
+#STATIC_ROOT = os.path.join(BASE_DIR,'/static') # untuk deploy
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
